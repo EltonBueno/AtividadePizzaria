17cb4fb3ea9e679c657110e609493ed6bc8e7c84
EltonCasa <elsbueno6@gmail.com>
2022-08-14 17:37:50 -0300

    Criação de Métodos e Models

---
+++ AtividadePizzaria/Models/PizzaSabor.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AtividadePizzaria.Models
+{
+    public class PizzaSabor
+    {
+        public PizzaSabor(int pizzaId, int saborId)
+        {
+            PizzaId = pizzaId;
+            SaborId = saborId;
+        }
+
+        [Key]
+        public int PizzaId { get; set; }
+        public Pizza Pizza { get; set; }
+
+        [Key]
+        public int SaborId { get; set; }
+        public Sabor Sabor { get; set; }
+
+    }
+}
